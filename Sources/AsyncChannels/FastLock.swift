94c460865df14ddcb113bd97037f4d66971b04db
Brian Floersch <gh123man@gmail.com>
2025-03-04 19:58:08 -0500

    try using a spin lock for linux

--- Sources/AsyncChannels/FastLock.swift
+++ Sources/AsyncChannels/FastLock.swift
@@ -2,27 +2,16 @@ import Foundation
 
 #if canImport(Darwin)
 class FastLock {
-    let unfairLock = {
-        let l = UnsafeMutablePointer<os_unfair_lock>.allocate(capacity: 1)
-        l.initialize(to: os_unfair_lock())
-        return l
-    }()
-    
-    deinit {
-        unfairLock.deinitialize(count: 1)
-        unfairLock.deallocate()
-    }
+    private var spinlock = OSSpinLock()
 
-    @inlinable
     @inline(__always)
     func lock() {
-        os_unfair_lock_lock(unfairLock)
+        OSSpinLockLock(&spinlock)
     }
-   
-    @inlinable
+
     @inline(__always)
     func unlock() {
-        os_unfair_lock_unlock(unfairLock)
+        OSSpinLockUnlock(&spinlock)
     }
 }
 
@@ -30,27 +19,50 @@ class FastLock {
 
 class FastLock {
 
-    var m: pthread_mutex_t = {
-        var m = pthread_mutex_t()
-        pthread_mutex_init(&m, nil)
-        return m
+    private var spinlock: pthread_spinlock_t = {
+        var lock = pthread_spinlock_t()
+        pthread_spin_init(&lock, 0)
+        return lock
     }()
-    
+
     deinit {
-        pthread_mutex_destroy(&m)
+        pthread_spin_destroy(&spinlock)
     }
 
-    @inlinable
     @inline(__always)
     func lock() {
-        pthread_mutex_lock(&m)
+        pthread_spin_lock(&spinlock)
     }
-    
-    @inlinable
+
     @inline(__always)
     func unlock() {
-        pthread_mutex_unlock(&m)
+        pthread_spin_unlock(&spinlock)
     }
+
+
+      /*   var m: pthread_mutex_t = {
+      var m = pthread_mutex_t()
+      var attr = pthread_mutexattr_t()
+      pthread_mutexattr_init(&attr)
+      pthread_mutexattr_settype(&attr, 3) // Faster under contention
+      pthread_mutex_init(&m, &attr)
+      pthread_mutexattr_destroy(&attr)
+      return m
+  }()
+
+  deinit {
+      pthread_mutex_destroy(&m)
+  }
+
+  @inline(__always)
+  func lock() {
+      pthread_mutex_lock(&m)
+  }
+
+  @inline(__always)
+  func unlock() {
+      pthread_mutex_unlock(&m)
+  }*/
 }
 
 #endif
